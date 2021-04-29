a901aabcf603684f37e5a31638fd7d0974b1b5b1
Shivangi <41469660+SHIVANGITHAKUR@users.noreply.github.com>
2021-04-29 11:24:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+
+#include <avr/io.h>
+
+int main(){
+    DDRD |= (1<<PD0); //Set the bit PD0 output
+	DDRB &= ~(1<<PB0); //clear the bit PB0 input
+	DDRC &= ~(1<<PC0); //clear the bit PC0 input
+    PORTB |= (1<<PB0);
+    PORTC |= (1<<PC0);
+
+    while(1){
+            if(((PINB & (1<<PB0))) && ((PINC & (1<<PC0)))){
+                PORTD &= ~(1<<PD0);
+            }
+            else if(((PINB & (1<<PB0))) && (!(PINC & (1<<PC0)))){
+                PORTD &= ~(1<<PD0);
+            }
+            else if((!(PINB & (1<<PB0))) && ((PINC & (1<<PC0)))){
+                PORTD &= ~(1<<PD0);
+            }
+            else if((!(PINB & (1<<PB0))) && (!(PINC & (1<<PC0)))){
+                PORTD |=  (1<<PD0);
+            }
+
+    }
+
+    return 0;
+}
